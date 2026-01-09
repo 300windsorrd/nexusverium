10e8dd7fb43f4a1a5223b2cb5f05427ee050b029
Eidan <Eidandreammer@gmail.com>
2026-01-09 02:15:02 -0500

    Contact form in working

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import { basePath } from "./src/lib/paths";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
